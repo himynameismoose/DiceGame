31b87e0b378cfd93af7b89e3141c68778c7dd1af
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2021-08-09 17:02:46 -0700

    Added a constructor for the LoadedDie class

--- src/LoadedDie.java
+++ src/LoadedDie.java
@@ -1,2 +1,19 @@
+/**
+ * The LoadedDie class simulates a weighted die.
+ *
+ * @author Mershelle Rivera
+ * @version 1.0
+ */
+
 public class LoadedDie {
+
+    /**
+     * The constructor performs an initial roll of the die.
+     *
+     * @param loadNumber            which number should come up more often
+     * @param moreTimesPerHundred   how many times per 100 rolls to come up with
+     *                              the loaded number (instead of uniform random)
+     */
+    public LoadedDie(int loadNumber, int moreTimesPerHundred) {
+    }
 }
