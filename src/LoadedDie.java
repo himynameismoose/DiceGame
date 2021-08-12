d88cf30636bee6d21120e06d2b95224bbae27795
Mershelle Rivera <mrivera21@mail.greenriver.edu>
2021-08-12 09:46:46 -0700

    Added a roll() method to simulate the rolling of a die

--- src/LoadedDie.java
+++ src/LoadedDie.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * The LoadedDie class simulates a weighted die.
  *
@@ -6,9 +8,14 @@
  */
 
 public class LoadedDie {
+    // Class constants
+    private static final int SIDES = 6;     // 6 sides of a die
+    private static final int RANGE = 100;   // Range to define the percentage dependent
+                                            // on whether a die lands on a loaded side
     // Class attributes
     private int favoredFace;    // The favored face of the die
     private int percentage;     // The percentage to represent more times per hundred
+    private int dieValue;       // The die's value after a roll
 
     /**
      * The constructor performs an initial roll of the die.
@@ -20,5 +27,30 @@ public class LoadedDie {
     public LoadedDie(int loadNumber, int moreTimesPerHundred) {
         favoredFace = loadNumber;
         percentage = moreTimesPerHundred;
+    }
+
+    /**
+     * The roll method simulates the rolling of the die.
+     * It will typically set this die's value to a random value
+     * with uniform distribution between 1 and 6. Occasionally,
+     * it will a priori return the favored value (with frequency
+     * determined by the moreTimesPerHundred argument that was passed
+     * to the constructor).
+     */
+    public int roll() {
+        // Create a Random object to be used to randomize a percentage
+        Random random = new Random();
+
+        // Randomize a percentage
+        int randomPercent = random.nextInt(RANGE) + 1; // Keep 100 inclusive
+
+        // If the randomPercent is less than the percentage determined in the constructor,
+        // then assign dieValue to favoredFace
+        if (randomPercent < percentage)
+            dieValue = favoredFace;
+        else                                        // Otherwise, the randomly roll the die
+            dieValue = random.nextInt(SIDES) + 1;   // to land on a value from 1-6.
+
+        return dieValue;
     }
 }
